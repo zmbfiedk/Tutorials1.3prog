3039608ca0305fd213b29c8813895c2935c78046
Arthur <lotuslovesurmom@gmail.com>
2025-04-11 11:44:56 +0200

    tutorial 7

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
-    private Rigidbody rb; 
-    public float force = 20f; //jump force keep public
+    private Rigidbody rb;
+    public float force = 20f;
+    [SerializeField] private float gravScaleUp = 1.5f;
+    [SerializeField] private float gravScaleDown = 3f;
+    private float originalGravity = -9.81f;
+
     void Start()
     {
-        rb = GetComponent<Rigidbody>();// dont delete
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -14,6 +18,15 @@ public class Jump : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector3.up * force, ForceMode.Impulse);
+        }
+
+        if (rb.velocity.y > 0)
+        {
+            Physics.gravity = new Vector3(0, gravScaleUp * originalGravity, 0);
+        }
+        else if (rb.velocity.y < 0)
+        {
+            Physics.gravity = new Vector3(0, gravScaleDown * originalGravity, 0);
         }
     }
 }
